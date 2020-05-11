7b68b09c6ee6bd8c8ce835513be6f3a7638a7327
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-11 10:31:18 -0700

    populated triangle type code

--- TriangleTest/Models/Triangle.cs
+++ TriangleTest/Models/Triangle.cs
@@ -2,6 +2,38 @@ namespace TriangleTest
 {
   public class Triangle
   {
-
+    public static string TriangleType(int side1, int side2, int side3)
+    {
+      if ((side1 + side2 < side3) || (side1 + side3 < side2) || (side2 + side3 < side1))
+      {
+        // Console.WriteLine("Invalid triangle!");
+        return "false";
+      }
+      else if (side1 == side2 && side2 == side3 && side1 == side3)
+      {
+        // Console.WriteLine("Equilateral");
+        return "false";
+      }
+      else if (side1 == side2 || side1 == side3 || side2 == side3)
+      {
+        // Console.WriteLine("Isosceles");
+        return "false";
+      }
+      else if (side1 != side2 && side2 != side3 && side1 != side3)
+      {
+        // Console.WriteLine("Scalene");
+        return "false";
+      }
+    }
+    public static void Main()
+    {
+      Console.WriteLine("Enter Side 1 Length: ");
+      int side1 = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter Side 2 Length: ");
+      int side2 = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter Side 3 Length: ");
+      int side3 = int.Parse(Console.ReadLine());
+      Console.WriteLine(TriangleType(side1, side2, side3));
+    }
   }
 }
